91a1086eb9709515f64de7eb73ee3cb30142b920
Onsite-health <jon@onsitehealthtx.com>
2024-09-03 14:32:04 -0500

    Delete Onsite_Webpage directory
    Deleted directory

--- Onsite_Webpage/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    document.getElementById('form-message').textContent = 'Thank you for contacting us! We will get back to you soon.';
-    this.reset();
-});
